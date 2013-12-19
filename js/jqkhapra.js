43cc0e71da8fb617a428842fbfeb3052cf8fd99e
Gaurav Ramanan <gaurav@rcorp.co.in>
2013-12-19 13:30:45 +0530

    Update: new paths to minified css and js

--- js/jqkhapra.js
+++ js/jqkhapra.js
@@ -24,19 +24,19 @@
 				 .sort().map(function( v ) { return v[ 1 ] })
 			: arr
 		}
-		
-		var o = $.extend( {}, _defaults, options);
+
+		var settings = $.extend( {}, _defaults, options);
 
 		return this.each(function() {
 
 			var $container = $(this);
 			var width = $container.find('img').width(),
-			height = $container.find('img').height(),//,o.img_height,
+			height = $container.find('img').height(),
 			$img = $('img', $container),
-			n_tiles = o.x * o.y,
+			n_tiles = settings.x * settings.y,
 			tiles = [], $tiles;
 			for ( var i = 0; i < n_tiles; i++ ) {
-				tiles.push('<div class="tile"><div class="card"><div class="card-front face"></div><div class="card-back face"></div></div></div>');
+				tiles.push('<div class="khapra-tile"><div class="khapra-card"><div class="khapra-card-front khapra-face"></div><div class="khapra-card-back khapra-face"></div></div></div>');
 			}
 
 			$tiles = $( tiles.join('') );
@@ -47,39 +47,39 @@
 			
 			// Set background
 			$tiles.css({
-				width: (100 / o.x) + '%',
-				height: height / o.y
+				width: (100 / settings.x) + '%',
+				height: height / settings.y
 			});
 
 			//Set the first image as the front side.
-			$('.card-front', $tiles).css('backgroundImage', 'url('+ $($img[0]).attr('src') +')');
+			$('.khapra-card-front', $tiles).css('backgroundImage', 'url('+ $($img[0]).attr('src') +')');
 
 			//Set the second image as the back side.
-			$('.card-back', $tiles).css('backgroundImage', 'url('+ $($img[1]).attr('src') +')');
+			$('.khapra-card-back', $tiles).css('backgroundImage', 'url('+ $($img[1]).attr('src') +')');
 
 			//Adjust size
-			var backgroundSizeX = (o.x * 100);
-			var backgroundSizeY = (o.y * 100);
-			$('.card-front, .card-back', this).css( 'backgroundSize', backgroundSizeX +'%'+ backgroundSizeY+'%' );
+			var backgroundSizeX = (settings.x * 100);
+			var backgroundSizeY = (settings.y * 100);
+			$('.khapra-card-front, .khapra-card-back', this).css( 'backgroundSize', backgroundSizeX +'%'+ backgroundSizeY+'%' );
 
 			// Adjust position
 			$tiles.each(function(e) {
 				var pos = $(this).position();
-				//var translateX = (e % o.x) * ((width + (37*width)/100) / o.x);
-				var translateX = (e % o.x) * (100/ (o.x-1));
-				var translateY = Math.floor(e / o.x) * (height / o.y);
+				//var translateX = (e % settings.x) * ((width + (37*width)/100) / settings.x);
+				var translateX = (e % settings.x) * (100/ (settings.x-1));
+				var translateY = Math.floor(e / settings.x) * (height / settings.y);
 			
-				$('.card-front, .card-back', this).css( 'backgroundPosition', translateX +'%'+ -translateY +'px' );
+				$('.khapra-card-front, .khapra-card-back', this).css( 'backgroundPosition', translateX +'%'+ -translateY +'px' );
 			});
 
-			var tilesArr = range( 0, n_tiles, o.random )
-				tileSpeed = o.speed / n_tiles; // time to clear a single tile
+			var tilesArr = range( 0, n_tiles, settings.random )
+				tileSpeed = settings.speed / n_tiles; // time to clear a single tile
 
 			// Public method
-			$container.on( 'animate', function() {
+			$container.on( 'khapra-flip', function() {
 				tilesArr.forEach(function( tile, i ) {
 					setTimeout(function(){
-						$tiles.eq( tile ).toggleClass( 'tile-flip' );
+						$tiles.eq( tile ).toggleClass( 'khapra-tile-flip' );
 					}, i * tileSpeed );
 				});
 			});
